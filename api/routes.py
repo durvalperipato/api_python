e4efd18467d91bfa4c5dcb7e023a0d543afe6174
Durval Peripato Neto <durvalperipatoneto@gmail.com>
2021-04-18 18:22:17 -0300

    Update

--- api/routes.py
+++ api/routes.py
@@ -2,9 +2,9 @@
 from flask_restful import Api
 
 # project resources
-#from api.authentication import SignUpApi, LoginApi
-#from api.user import UsersApi, UserApi
-#from api.meal import MealsApi, MealApi
+from api.authentication import SignUpApi, LoginApi
+from api.user import UsersApi, UserApi
+from api.meal import MealsApi, MealApi
 from api.home import HomeApi
 
 
@@ -16,13 +16,13 @@ def create_routes(api: Api):
         api.add_resource(Foo, '/foo', endpoint="foo")
         api.add_resource(FooSpecial, '/special/foo', endpoint="foo")
     """
-    #api.add_resource(SignUpApi, '/authentication/signup/')
-    #api.add_resource(LoginApi, '/authentication/login/')
+    api.add_resource(SignUpApi, '/authentication/signup/')
+    api.add_resource(LoginApi, '/authentication/login/')
 
-    #api.add_resource(UsersApi, '/user/')
-    #api.add_resource(UserApi, '/user/<user_id>')
+    api.add_resource(UsersApi, '/user/')
+    api.add_resource(UserApi, '/user/<user_id>')
 
-    #api.add_resource(MealsApi,'/meal/')
-    #api.add_resource(MealApi, '/meal/<meal_id>')
+    api.add_resource(MealsApi,'/meal/')
+    api.add_resource(MealApi, '/meal/<meal_id>')
 
     api.add_resource(HomeApi, '/')
